5ff4880fee50f631af95bf9e6277d8896fba6802
Youssouf Tangara <ytangara2003@gmail.com>
2024-12-27 14:48:22 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ensetproject.chatbotedu {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ensetproject.chatbotedu to javafx.fxml;
+    exports ensetproject.chatbotedu;
+}
